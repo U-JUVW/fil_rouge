4ba9a1dba53e096c870b1f2a41b8d23af37f919f
hugobarauj <76562430+hugobarauj@users.noreply.github.com>
2022-01-10 20:40:06 +0100

    10/01 - 20h40 | Ajout des commentaires

--- list.h
+++ list.h
@@ -3,12 +3,13 @@
 #ifndef _LIST_H_
 #define _LIST_H_
 
+//Structure of a standard linked list
 typedef struct node {
 	_element data;
 	struct node *pNext;
 } _listNode, * _list;
 
-
+//Prototypes for linked lists
 _list addNode (_element e, _list n);
 void showList(_list l);
 int getLength(_list l);
