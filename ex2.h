0349a3448fee5985049386790d650681c84b32cc
R-GRST <96395371+R-GRST@users.noreply.github.com>
2022-01-10 14:47:51 +0100

    Update ex2.h

--- ex2.h
+++ ex2.h
@@ -1,4 +1,4 @@
 //Prototypes
 void program2(char * path);
-void printStats(int length, int nbWords, double timeExec, int nbNodes, int hMin);
+void printStats(int length, int nbWords, double timeExec, int nbNodes, int hMin, int hAVL);
 void getAnagrams(char * input, _avlTree root);
